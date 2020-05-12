24726df5897a88fe7f7100290b2191eccc53cef0
Ashutosh Chauhan <masterashu@live.in>
2020-05-12 17:32:19 +0530

    add web support and demo

---
+++ android/app/src/main/kotlin/com/masterashu/animmated_bottom_nav_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.masterashu.animmated_bottom_nav_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ docs/flutter_service_worker.js
@@ -0,0 +1,40 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "8440e5c8f0cd33813cc3052dc07c10ed",
+"/": "8440e5c8f0cd33813cc3052dc07c10ed",
+"main.dart.js": "6ef0c4570e85b9b2f99adce21ec87d6d",
+"manifest.json": "eea66bcb0d24721ca40256a367e9a074"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
